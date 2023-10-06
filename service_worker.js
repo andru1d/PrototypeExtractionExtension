76780290d2559ce9d1eb492f6287388d0d6b2244
andru1d <andruid@ecologylab.net>
2023-10-06 14:18:45 -0500

    Add debug statement to service_worker. Minor wrapper improvement. Change name to PrototypeExtraction in manifest.

--- service_worker.js
+++ service_worker.js
@@ -3,29 +3,41 @@
 import { wrappers } from './wrappers.js';
 
 function is_selected(url, selector) {
+    console.log(`url.host ${url.host} ${selector.host}`);
   if (selector.host !== url.host) return false
   if (!selector.path_regex.test(url.pathname)) return false
   return true
 }
 
-function find_wrapper(url) {
-  if (!url) return null
-  for (let i = 0; i < wrappers.length; ++i) {
-    let wrapper = wrappers[i]
-    for (let j = 0; j < wrapper.selector.length; ++j) {
-      if (is_selected(url, wrapper.selector[j])) {
-        return wrapper
-      }
+function find_wrapper(url)
+{
+    if (!url) 
+       return null;
+    let result = null;
+    for (let i = 0; i < wrappers.length; ++i) 
+    {
+        let wrapper = wrappers[i]
+        for (let j = 0; j < wrapper.selector.length; ++j) 
+        {
+            if (wrapper.name == "default")
+            {
+                result = wrapper;
+                    
+            }
+            else if (is_selected(url, wrapper.selector[j])) 
+            {
+                return wrapper
+            }
+        }
     }
-  }
-  return null
+    return result;
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.query_wrapper) {
     let wrapper = find_wrapper(message.query_wrapper.location)
     console.log('wrapper found for url ' +
-      message.query_wrapper.location.toString() + ': ', wrapper)
+      message.query_wrapper.location.href + ': ', wrapper)
     sendResponse({
       wrapper: wrapper
     })
--- wrappers.js
+++ wrappers.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 export const wrappers = [
   {
@@ -25,7 +25,9 @@ export const wrappers = [
       {
         name: 'price',
         xpath: [
-          '//*[contains(@class, "priceToPay")]'
+            '//input[@id="twister-plus-price-data-price"]/@value'
+    //        '//span[@class="a-offscreen"]/text()[1]'
+          // '//*[contains(@class, "priceToPay")]'
         ]
       },
       {
@@ -35,5 +37,96 @@ export const wrappers = [
         ]
       },
     ]
-  }
-]
+  },
+{
+    name: 'google_search',
+    selector: [
+      {
+        host: 'www.google.com',
+        path_regex: new RegExp('^/search'),
+      }
+    ],
+    fields: [
+      {
+        name: 'query',
+        xpath: [
+          '//textarea/@value'
+        ]
+      },
+      {
+        name: 'search_result',
+        xpath: [
+          '//div[@id="search"]//div[@class="MjjYud"]'
+        ],
+        fields: [
+            {
+                name: 'search_title',
+                xpath: [
+                    '//h3/text(1)'
+                ]
+            },
+            {
+                name: 'search_link',
+                xpath: [
+                    '//a/@href'
+                ]
+            },
+            {
+                name: 'search_snippet',
+                xpath: []
+            },
+            {
+                name: 'search_favicon',
+                xpath: [
+                    '//img/@src'
+                ]
+            }
+        ]
+      },
+    ]
+  },
+  {
+    name: 'default',
+    selector: [
+      {
+        host: '',
+        path_regex: new RegExp(),
+      }
+    ],
+    fields: [
+      {
+        name: 'og_type',
+        xpath: [
+            '/html/head/meta[@property="og:type"]/@content',
+            '/html/head/meta[@name="dc.Type"]/@content'
+      ]
+      },
+      {
+        name: 'title',
+        xpath: [
+            '/html/head/title/text()[1]',
+            '/html/head/meta[@property="og:title"]/@content'
+
+        ]
+      },
+      {
+        name: 'description',
+        xpath: [
+          '/html/head/meta[@property="og:description"]/@content',
+        ]
+      },
+      {
+        name: 'image',
+        xpath: [
+           '/html/head/meta[@property="og:image"]/@content'
+       ]
+      },
+      {
+        name: 'keywords', // name=keywords or property = news_keywords
+        xpath: [
+            '/html/head/meta[@name="news_keywords"]/@content',
+            '/html/head/meta[@name="keywords"]/@content'
+        ]
+      },
+    ]
+  }]
